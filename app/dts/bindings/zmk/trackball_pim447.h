f553971fcb00e000eed9fb685c2401d784ffc1cb
Lee-Seokjeong <154737737+Lee-Seokjeong@users.noreply.github.com>
2024-08-07 02:07:12 +0900

    Create trackball_pim447.h

---
+++ app/dts/bindings/zmk/trackball_pim447.h
@@ -0,0 +1,9 @@
+// zmk/app/dts/bindings/zmk/trackball_pim447.h
+
+#ifndef ZMK_TRACKBALL_PIM447_H
+#define ZMK_TRACKBALL_PIM447_H
+
+#define PIM447_MOVE 0
+#define PIM447_SCROLL 1
+
+#endif // ZMK_TRACKBALL_PIM447_H
